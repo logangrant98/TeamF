5c918fa372bda57c032d6e200457ad2de1f433d6
logangrant98 <logangrant98@users.noreply.github.com>
2020-11-22 18:05:30 -0600

    Properties and Database

--- src/edu/jsu/mcis/tas_fa20/TASDatabase.java
+++ src/edu/jsu/mcis/tas_fa20/TASDatabase.java
@@ -5,10 +5,23 @@
  */
 package edu.jsu.mcis.tas_fa20;
 
+import java.sql.*;
+
 /**
  *
  * @author Logan
  */
 public class TASDatabase {
+    public static void main(String args[]){
+         try{
+             Class.forName("com.mysql.jdbc.Driver");
+             Connection con = DriverManager.getConnection(
+                     "jdbc:mysql://localhost:3306/Tas" , "tasuser" , "cs310groupC");
+         } /*this will get us the connection to the database itself, where we will
+         be able to get information from the tables */
+         catch(Exception e) {
+             System.out.println(e);
+         }
+     }
     
 }
